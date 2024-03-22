805237a42ad9ae9abb7ea00ff99dd4a8a2348759
TvorezReal <sentr777@bk.ru>
2024-03-22 12:46:35 +0300

    add new const

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import pygame
 
-
 pygame.init()
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))  # кортеж размеров экрана
+
+pygame.display.set_caption("Игра Тир")  # заголовок окна
 
 running = True
 while running:
